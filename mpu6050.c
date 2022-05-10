1bb8802103fd33c4aa068283decd7e956f1b4f63
Thareejan <thareejan@gmail.com>
2022-05-11 00:14:28 +0530

    modulized

--- mpu6050.c
+++ mpu6050.c
@@ -1,49 +1,50 @@
-#define F_CPU 8000000UL		/* Define CPU clock Frequency 8MHz */
-#include <avr/io.h>		/* Include AVR std. library file */
-#include <util/delay.h>		/* Include delay header file */
-#include <inttypes.h>		/* Include integer type header file */
-#include <stdlib.h>		/* Include standard library file */
-#include <stdio.h>		/* Include standard I/O library file */
-#include "MPU6050_res_define.h"	/* Include MPU6050 register define file */
-#include "I2C_Master_H_file.h"	/* Include I2C Master header file */
-#include "USART_RS232_H_file.h"	/* Include USART header file */
+#define F_CPU 8000000UL									/* Define CPU clock Frequency e.g. here its 8MHz */
+#include <avr/io.h>										/* Include AVR std. library file */
+#include <util/delay.h>									/* Include delay header file */
+#include <inttypes.h>									/* Include integer type header file */
+#include <stdlib.h>										/* Include standard library file */
+#include <stdio.h>										/* Include standard library file */
+#include "MPU6050_res_define.h"							/* Include MPU6050 register define file */
+#include "I2C_Master_H_file.h"							/* Include I2C Master header file */
+#include "USART_RS232_H_file.h"							/* Include USART header file */
 
-float Acc_x,Acc_y,Acc_z,Temperature,Gyro_x,Gyro_y,Gyro_z;
+float Acc_x,Acc_y,Acc_z,Gyro_x,Gyro_y,Gyro_z;
+int arr[2];
 
-void Gyro_Init()		/* Gyro initialization function */
+void MPU6050_Init()										/* Gyro initialization function */
 {
-	_delay_ms(150);		/* Power up time >100ms */
-	I2C_Start_Wait(0xD0);	/* Start with device write address */
-	I2C_Write(SMPLRT_DIV);	/* Write to sample rate register */
-	I2C_Write(0x07);	/* 1KHz sample rate */
+	_delay_ms(150);										/* Power up time >100ms */
+	I2C_Start_Wait(0xD0);								/* Start with device write address */
+	I2C_Write(SMPLRT_DIV);								/* Write to sample rate register */
+	I2C_Write(0x07);									/* 1KHz sample rate */
 	I2C_Stop();
 
 	I2C_Start_Wait(0xD0);
-	I2C_Write(PWR_MGMT_1);	/* Write to power management register */
-	I2C_Write(0x01);	/* X axis gyroscope reference frequency */
+	I2C_Write(PWR_MGMT_1);								/* Write to power management register */
+	I2C_Write(0x01);									/* X axis gyroscope reference frequency */
 	I2C_Stop();
 
 	I2C_Start_Wait(0xD0);
-	I2C_Write(CONFIG);	/* Write to Configuration register */
-	I2C_Write(0x00);	/* Fs = 8KHz */
+	I2C_Write(CONFIG);									/* Write to Configuration register */
+	I2C_Write(0x00);									/* Fs = 8KHz */
 	I2C_Stop();
 
 	I2C_Start_Wait(0xD0);
-	I2C_Write(GYRO_CONFIG);	/* Write to Gyro configuration register */
-	I2C_Write(0x18);	/* Full scale range +/- 2000 degree/C */
+	I2C_Write(GYRO_CONFIG);								/* Write to Gyro configuration register */
+	I2C_Write(0x18);									/* Full scale range +/- 2000 degree/C */
 	I2C_Stop();
 
 	I2C_Start_Wait(0xD0);
-	I2C_Write(INT_ENABLE);	/* Write to interrupt enable register */
+	I2C_Write(INT_ENABLE);								/* Write to interrupt enable register */
 	I2C_Write(0x01);
 	I2C_Stop();
 }
 
 void MPU_Start_Loc()
 {
-	I2C_Start_Wait(0xD0);	/* I2C start with device write address */
-	I2C_Write(ACCEL_XOUT_H);/* Write start location address from where to read */
-	I2C_Repeated_Start(0xD1);/* I2C start with device read address */
+	I2C_Start_Wait(0xD0);								/* I2C start with device write address */
+	I2C_Write(ACCEL_XOUT_H);							/* Write start location address from where to read */
+	I2C_Repeated_Start(0xD1);							/* I2C start with device read address */
 }
 
 void Read_RawValue()
@@ -59,39 +60,60 @@ void Read_RawValue()
 	I2C_Stop();
 }
 
-int mpu6050()
+
+int*  positionLogic()
 {
-	char buffer[20], float_[10];
+	int right=1,left=2,up=3,down=4;
+	int position = right ,position2=up;
 	float Xa,Ya,Za,t;
 	float Xg=0,Yg=0,Zg=0;
-	I2C_Init();		/* Initialize I2C */
-	Gyro_Init();		/* Initialize Gyro */
-	USART_Init(9600);	/* Initialize USART with 9600 baud rate */
+	I2C_Init();											/* Initialize I2C */
+	MPU6050_Init();										/* Initialize MPU6050 */
+	USART_Init(9600);									/* Initialize USART with 9600 baud rate */
 
 	while(1)
 	{
 		Read_RawValue();
 
-		/* Divide raw value by sensitivity scale factor */
-		Xa = Acc_x/16384.0;
+		Xa = Acc_x/16384.0;								/* Divide raw value by sensitivity scale factor to get real values */
 		Ya = Acc_y/16384.0;
 		Za = Acc_z/16384.0;
 
 		Xg = Gyro_x/16.4;
 		Yg = Gyro_y/16.4;
 		Zg = Gyro_z/16.4;
-
-		/* Convert temperature in /c using formula */
-		t = (Temperature/340.00)+36.53;
+		if (position==right)
+		{
+			if(Xa>0)
+				position=left;
+			else
+				position=right;
+		}
+		else if(position==left)
+		{
+			if(Xa<0)
+				position=right
+			else
+				position=left;
+		}
+		if (position2==down)
+		{
+			if(Ya>0)
+				position2=up;
+			else
+				position2=down;
+		}
+		else if (position2==up)
+		{
+			if(Ya<0)
+				position2=down;
+			else
+				position2=up;
+		}
+		_delay_ms(200);
 
 	}
+	arr[1]=position;
+	arr[2]=position2;
+	return arr;
 }
-
-if(Xg < 0.0)
-		{
-			right();
-		}
-		else if(Xg > 1.5)
-		{
-			left();
-		}
