2e8b000209248ca583aeefcaa09f40bf4faf7646
Tharanika11 <86451347+Tharanika11@users.noreply.github.com>
2022-05-11 02:09:15 +0530

    Update FsrGripperFunction.c

--- FsrGripperFunction.c
+++ FsrGripperFunction.c
@@ -5,7 +5,7 @@ float getVoltage(float val)
 	return (val * 4.82 / 255.0);
 }
 
-void rotateZero()
+void stopGripper()
 {
 	_delay_ms(1500);
 	pin = 175;	/* Set servo shaft at 0° position */
@@ -13,14 +13,9 @@ void rotateZero()
 }
 
 
-void rotateNinty()
+void pushGripper()
 {
 	pin = 300;	/* Set servo at +90° position */
 	_delay_ms(1500);
 }
 
-void rotateMinusNinty()
-{
-	pin = 65;	/* Set servo shaft at -90° position */
-	_delay_ms(1500);
-}
