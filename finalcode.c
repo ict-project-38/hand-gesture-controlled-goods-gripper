63f0bcdfe7a85d34c1a5f4d472ce26318173f0b0
Thareejan <thareejan@gmail.com>
2022-05-11 01:51:32 +0530

    Finished MPU6050-Stepper Integration

--- finalcode.c
+++ finalcode.c
@@ -1,12 +1,35 @@
-#include "mpu6050.c"
+#include "Mpu6050Input.c"
 #include "ServoFlexFunctions.c"
+#include "StepperFunctions.c"
+#include "TriggerFunction.c"
 
 
 
 
-int int main() {
-	if (*positionLogic()==1)
+int int main()
+{
+	void loop()
 	{
+		if ((*positionLogic()==5)&& trigger==0))
+		{
+			StepStop();
+		}
+		else if ((*positionLogic()==1)&& trigger==0))
+		{
+			HorizontalStepRight();
+		}
+		else if((*positionLogic()==2)&& trigger==0)
+		{
+			HorizontalStepLeft();
+		}
+		else if((*(positionLogic()+2)==3)&& trigger==0))
+		{
+			VerticalStepUp();
+		}
+		else if((*(positionLogic()+2)==4)&& trigger==0))
+		{
+			VerticalStepDown();
+		}
 
 	}
 	return 0;
