8909b744429a738b252889c48c3284ccb95f7e32
Danuraha <95142326+Danuraha@users.noreply.github.com>
2022-05-11 00:19:01 +0530

    Add files via upload

---
+++ ServoFlexFunctions.c
@@ -0,0 +1,26 @@
+#include <util/delay.h>
+int pin;
+float getVoltage(float val)
+{
+	return (val * 4.82 / 255.0);
+}
+
+void rotateZero()
+{
+	_delay_ms(1500);
+	pin = 175;	/* Set servo shaft at 0° position */
+	_delay_ms(1500);
+}
+
+
+void rotateNinty()
+{
+	pin = 300;	/* Set servo at +90° position */
+	_delay_ms(1500);
+}
+
+void rotateMinusNinty()
+{
+	pin = 65;	/* Set servo shaft at -90° position */
+	_delay_ms(1500);
+}
