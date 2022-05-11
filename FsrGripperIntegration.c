9783d77d9bd83c66c1425cc7caa606e830867885
Tharanika11 <86451347+Tharanika11@users.noreply.github.com>
2022-05-11 13:46:10 +0530

    Update FsrGripperIntegration.c

--- FsrGripperIntegration.c
+++ FsrGripperIntegration.c
@@ -2,45 +2,25 @@
 #include <avr/io.h>		
 #include <stdio.h>		
 #include <util/delay.h>		
-
-
 void loop()
 {
 	while(1)
 	{
-
 		ADCSRA |= (1 << ADSC);
 		while (ADCSRA & (1 << ADSC))
 		;
 		float tem, volt;
 		tem = ADCH * 1.0;
 		volt = getVoltage(tem);
-		switch (ADMUX)
-		{
-			case 0X40:
-			if (volt >= 1.6)
+	
+			if (volt >= 5)
 			{
-
+				
 			}
-			else if (volt < 1.6)
+			else if (volt < 5)
 			{
-
+				
 			}
-
-			ADMUX = 0X87;
-			break;
-			case 0X87:
-			if (volt >= 1.5)
-			{
-
-			}
-			else if (volt < 1.5)
-			{
-
-			}
-
-			ADMUX = 0X40;
-			break;
 		}
 		_delay_ms(500);
 	}
