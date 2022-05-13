2d177f70275c971f370436019bfd43fd8d44cddc
Danuraha <95142326+Danuraha@users.noreply.github.com>
2022-05-13 17:02:38 +0530

    setup part removed

--- FlexServoIntegration.c
+++ FlexServoIntegration.c
@@ -11,5 +11,10 @@ void setup()
     tem = reg3 * 1.0;
     volt = getVoltage(tem);
 
-	
+	TCNT1 = 0;		/* Set timer1 count zero */
+	ICR1 = 19999;		/* Set TOP count for timer1 in ICR1 register */
+	DDRD |= (1<<PD5);
+	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
+	TCCR1A = (1<<WGM11)|(1<<COM1A1);
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);	
 }
