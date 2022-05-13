1035d3806bf889a451104153cededc58b6326137
Thareejan <thareejan@gmail.com>
2022-05-13 16:12:18 +0530

    More integration

--- main.c
+++ main.c
@@ -2,19 +2,37 @@
 #include "ServoFlexFunctions.c"
 #include "StepperFunctions.c"
 #include "TriggerFunction.c"
+#include "UltrasonicFunction.c"
+#include "GripperFunction.c"
+#include "BluetoothFunctions.c"
 
 
 
+
+void Mpu6050Stepper();
+void Mpu6050Servo();
+void gripper();
 
 int main()
 {
 	void loop()
 	{
-		if ((*positionLogic()==5)&& trigger==0))
+		Mpu6050Stepper();
+		Mpu6050Servo();
+		
+
+	}
+	return 0;
+}
+
+
+void Mpu6050Stepper()
+{
+	if ((*positionLogic()==5)&& trigger==0)
 		{
 			StepStop();
 		}
-		else if ((*positionLogic()==1)&& trigger==0))
+		else if ((*positionLogic()==1)&& trigger==0)
 		{
 			HorizontalStepRight();
 		}
@@ -22,15 +40,43 @@ int main()
 		{
 			HorizontalStepLeft();
 		}
-		else if((*(positionLogic()+2)==3)&& trigger==0))
+		else if((*(positionLogic()+2)==3)&& trigger==0)
 		{
 			VerticalStepUp();
 		}
-		else if((*(positionLogic()+2)==4)&& trigger==0))
+		else if((*(positionLogic()+2)==4)&& trigger==0)
 		{
 			VerticalStepDown();
 		}
+}
 
-	}
-	return 0;
+void Mpu6050Servo()
+{
+	if ((*positionLogic()==5)&& trigger==1)
+		{
+			Servo1Stop();
+			Servo3Stop();
+		}
+		else if ((*positionLogic()==1)&& trigger==1)
+		{
+			Servo1Right();
+		}
+		else if((*positionLogic()==2)&& trigger==1)
+		{
+			Servo1Left();
+		}
+		if((*(positionLogic()+2)==3)&& trigger==1)
+		{
+			Servo3up();
+		}
+		else if((*(positionLogic()+2)==4)&& trigger==1)
+		{
+			Servo3Down();
+		}
+
+}
+
+void gripper()
+{
+
 }
