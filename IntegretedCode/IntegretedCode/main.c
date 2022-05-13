8e3cdbbaa6638f6b0e04fe52248d74b5be6af996
Thareejan <thareejan@gmail.com>
2022-05-13 16:13:28 +0530

    Integration

---
+++ IntegretedCode/IntegretedCode/main.c
@@ -0,0 +1,18 @@
+/*
+ * IntegretedCode.c
+ *
+ * Created: 5/13/2022 3:43:49 PM
+ * Author : Thareejan
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
