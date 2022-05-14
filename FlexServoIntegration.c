2b25432d248950eff51fb5b054f15a54df13dba8
Danuraha <95142326+Danuraha@users.noreply.github.com>
2022-05-14 16:50:58 +0530

    flexservoInte-2

--- FlexServoIntegration.c
+++ FlexServoIntegration.c
@@ -1,7 +1,12 @@
-#define F_CPU 1000000UL		/* Define CPU Frequency e.g. here its 8MHz */
-#include <avr/io.h>		/* Include AVR std. library file */
-#include <stdio.h>		/* Include std. library file */
-#include <util/delay.h>		/* Include Delay header file */
+
+#define F_CPU 8000000UL		/* define Clock Frequency */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
 
 void setup()
 {
@@ -12,10 +17,11 @@ void setup()
     volt = getVoltage(tem);
 
 	TCNT1 = 0;		/* Set timer1 count zero */
-	ICR1 = 19999;		/* Set TOP count for timer1 in ICR1 register */
-	DDRD |= (1<<PD5);
+	ICR1 = 2499;		/* Set TOP count for timer1 in ICR1 register */
+
 	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
-	TCCR1A = (1<<WGM11)|(1<<COM1A1);
-	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);	
+	TCCR1A = (1<<WGM11)|(1<<COM1A1)|(1<<COM1B1);
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
+	DDRD = 0b10110000;
 
 }
