a8a3afc626afe02fdd10748b95dda8783ad42366
Thareejan <thareejan@gmail.com>
2022-05-14 16:49:17 +0530

    Updated Build Files(Binary Files)

--- MoavleArmSystem/MoavleArmSystem/main.c
+++ MoavleArmSystem/MoavleArmSystem/main.c
@@ -4,48 +4,36 @@
  * Created: 5/13/2022 6:11:43 PM
  * Author : Thareejan
  */ 
+#define F_CPU 8000000UL		/* define Clock Frequency */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
 
-//#include <avr/io.h>
 
-
-#ifndef F_CPU#define F_CPU 8000000UL#endif	/* Define CPU Frequency e.g. here its 8MHz */
-#include <avr/io.h>		/* Include AVR std. library file */
-#include <stdio.h>		/* Include std. library file */
-#include <util/delay.h>		/* Include Delay header file */
-unsigned int duty;	
- int servoSetup()
+int main()
 {
-	
+	unsigned int i;
 	TCNT1 = 0;		/* Set timer1 count zero */
-	ICR1 = 19999;		/* Set TOP count for timer1 in ICR1 register */
-	DDRD |= (1<<PD5);
-	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
-	TCCR1A = (1<<WGM11)|(1<<COM1A1);
-	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);	
-	}
-	
-	int main(void)
-	{
-		servoSetup();
-	
-	while(1)
-	{	
-		
-		for(duty=14; duty<32; duty++)   // 0 to max duty cycle
-		{
-			OCR1A=duty;
-			_delay_ms(1000);
-		//	OCR1A=0x00; 
+	ICR1 = 2499;		/* Set TOP count for timer1 in ICR1 register */
 
-		}
-		
-		for(duty=31; duty>=14; duty--)   // max to 0 duty cycle
-		{
-			OCR1A=duty;
-			_delay_ms(1000);
-		}
-		
-	
+	/* Set Fast PWM, TOP in ICR1, Clear OC1A on compare match, clk/64 */
+	TCCR1A = (1<<WGM11)|(1<<COM1A1)|(1<<COM1B1);
+	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
+	DDRD = 0b10110000;
+	while(1)
+	{
+	for(i=125;i<=250;i++)
+	{	
+	_delay_ms(1000);
+	OCR1B = i;
+	OCR1A = i;
+	OCR2  =i;
 	}
-	
+	}
+	return 0;
+
 }
