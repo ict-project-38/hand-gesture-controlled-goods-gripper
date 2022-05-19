b8df7a27e595446369cbf80e91be009ff5891b56
ThanusiyanS <86287092+ThanusiyanS@users.noreply.github.com>
2022-05-19 17:54:45 +0530

    Update GloveControlerIntegretedCode.c

--- GloveControlerIntegretedCode.c
+++ GloveControlerIntegretedCode.c
@@ -89,5 +89,17 @@ void Mpu6050Servo()
 
 void gripper()
 {
+	voltage[] = getVoltage();
+    int finalVoltage;
+    finalVoltage=(voltage[0]+voltage[1])/2;
+    if (finalVoltage>=342)
+    {
+      shrinkGripper;
+    }  
+    else
+    {
+      expandGripper;
+    }                                                            //30 degree--50k----1.67V
+    _delay_ms(500);
 
 }
