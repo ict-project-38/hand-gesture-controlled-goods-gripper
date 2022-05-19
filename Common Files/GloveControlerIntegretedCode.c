873d00e68982dd2fc245467b3ac66e55c3b244c0
Thareejan <thareejan@gmail.com>
2022-05-19 18:29:27 +0530

    Moved to new folder

---
+++ Common Files/GloveControlerIntegretedCode.c
@@ -0,0 +1,105 @@
+#include "ServoFlexFunctions.c"
+#include "StepperFunctions.c"
+#include "UltrasonicFunction.c"
+#include "GripperFunction.c"
+#include "BluetoothFunctions.c"
+
+
+
+
+void Mpu6050Stepper();
+void Mpu6050Servo();
+void gripper();
+
+int main()
+{
+	void loop()	
+	{
+		if (ultrasonicValue()>10)
+		{
+			Mpu6050Stepper();
+			Mpu6050Servo();
+		}
+		else
+		{
+			if (fsrValue()<5)
+				gripper();
+			else
+				gripperStop();
+			if(*(getSensorValues()+6)==0)
+				Mpu6050Stepper();
+		
+		}
+		
+
+	}
+	return 0;
+}
+
+
+void Mpu6050Stepper()
+{
+	if ((*getSensorValues()==5)&& (*(getSensorValues()+6))==0)
+		{
+			StepStop();
+		}
+		else if ((*getSensorValues()==1)&& (*(getSensorValues()+6))==0)
+		{
+			HorizontalStepRight();
+		}
+		else if((*getSensorValues()==2)&& (*(getSensorValues()+6))==0)
+		{
+			HorizontalStepLeft();
+		}
+		else if((*(getSensorValues()+2)==3)&& (*(getSensorValues()+6))==0)
+		{
+			VerticalStepUp();
+		}
+		else if((*(getSensorValues()+2)==4)&& (*(getSensorValues()+6))==0)
+		{
+			VerticalStepDown();
+		}
+}
+
+void Mpu6050Servo()
+{
+	if ((*(getSensorValues()+3)==5)&& (*(getSensorValues()+6))==1)
+		{
+			Servo1Stop();
+			Servo3Stop();
+		}
+		else if ((*getSensorValues()==1)&& (*(getSensorValues()+6))==1)
+		{
+			Servo1Right();
+		}
+		else if((*getSensorValues()==2)&& (*(getSensorValues()+6))==1)
+		{
+			Servo1Left();
+		}
+		if((*(getSensorValues()+2)==3)&& (*(getSensorValues()+6))==1)
+		{
+			Servo3up();
+		}
+		else if((*(getSensorValues()+2)==4)&& (*(getSensorValues()+6))==1)
+		{
+			Servo3Down();
+		}
+
+}
+
+void gripper()
+{
+	voltage[] = getVoltage();
+    int finalVoltage;
+    finalVoltage=(voltage[0]+voltage[1])/2;
+    if (finalVoltage>=342)
+    {
+      shrinkGripper;
+    }  
+    else
+    {
+      expandGripper;
+    }                                                            //30 degree--50k----1.67V
+    _delay_ms(500);
+
+}
---
+++ Common Files/Mpu6050Input.c
@@ -0,0 +1,120 @@
+#define F_CPU 8000000UL									/* Define CPU clock Frequency e.g. here its 8MHz */
+#include <avr/io.h>										/* Include AVR std. library file */
+#include <util/delay.h>									/* Include delay header file */
+#include <inttypes.h>									/* Include integer type header file */
+#include <stdlib.h>										/* Include standard library file */
+#include <stdio.h>										/* Include standard library file */
+#include "MPU6050_res_define.h"							/* Include MPU6050 register define file */
+#include "I2C_Master_H_file.h"							/* Include I2C Master header file */
+#include "USART_RS232_H_file.h"							/* Include USART header file */
+
+float Acc_x,Acc_y,Acc_z,Gyro_x,Gyro_y,Gyro_z;
+char arr[3];
+
+void MPU6050_Init()										/* Gyro initialization function */
+{
+	_delay_ms(150);										/* Power up time >100ms */
+	I2C_Start_Wait(0xD0);								/* Start with device write address */
+	I2C_Write(SMPLRT_DIV);								/* Write to sample rate register */
+	I2C_Write(0x07);									/* 1KHz sample rate */
+	I2C_Stop();
+
+	I2C_Start_Wait(0xD0);
+	I2C_Write(PWR_MGMT_1);								/* Write to power management register */
+	I2C_Write(0x01);									/* X axis gyroscope reference frequency */
+	I2C_Stop();
+
+	I2C_Start_Wait(0xD0);
+	I2C_Write(CONFIG);									/* Write to Configuration register */
+	I2C_Write(0x00);									/* Fs = 8KHz */
+	I2C_Stop();
+
+	I2C_Start_Wait(0xD0);
+	I2C_Write(GYRO_CONFIG);								/* Write to Gyro configuration register */
+	I2C_Write(0x18);									/* Full scale range +/- 2000 degree/C */
+	I2C_Stop();
+
+	I2C_Start_Wait(0xD0);
+	I2C_Write(INT_ENABLE);								/* Write to interrupt enable register */
+	I2C_Write(0x01);
+	I2C_Stop();
+}
+
+void MPU_Start_Loc()
+{
+	I2C_Start_Wait(0xD0);								/* I2C start with device write address */
+	I2C_Write(ACCEL_XOUT_H);							/* Write start location address from where to read */
+	I2C_Repeated_Start(0xD1);							/* I2C start with device read address */
+}
+
+void Read_RawValue()
+{
+	MPU_Start_Loc();									/* Read Gyro values */
+	Acc_x = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Acc_y = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Acc_z = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Temperature = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Gyro_x = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Gyro_y = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Ack());
+	Gyro_z = (((int)I2C_Read_Ack()<<8) | (int)I2C_Read_Nack());
+	I2C_Stop();
+}
+
+
+char*  positionLogic()
+{
+	char right=1,left=2,up=3,down=4,stable=5;
+	char position = right ,position2=up;
+	float Xa,Ya,Za;
+	float Xg=0,Yg=0,Zg=0;
+	I2C_Init();											/* Initialize I2C */
+	MPU6050_Init();										/* Initialize MPU6050 */
+	USART_Init(9600);									/* Initialize USART with 9600 baud rate */
+
+	while(1)
+	{
+		Read_RawValue();
+
+		Xa = Acc_x/16384.0;								/* Divide raw value by sensitivity scale factor to get real values */
+		Ya = Acc_y/16384.0;
+		Za = Acc_z/16384.0;
+
+		Xg = Gyro_x/16.4;
+		Yg = Gyro_y/16.4;
+		Zg = Gyro_z/16.4;
+		if (position==right)
+		{
+			if(Xa>stable)
+				position=left;
+			else
+				position=right;
+		}
+		else if(position==left)
+		{
+			if(Xa<stable)
+				position=right
+			else
+				position=left;
+		}
+		if (position2==down)
+		{
+			if(Ya>stable)
+				position2=up;
+			else
+				position2=down;
+		}
+		else if (position2==up)
+		{
+			if(Ya<stable)
+				position2=down;
+			else
+				position2=up;
+		}
+		_delay_ms(200);
+
+	}
+	arr[0]=position;
+	arr[1]=position2;
+	arr[2]=stable;
+	return arr;
+}
