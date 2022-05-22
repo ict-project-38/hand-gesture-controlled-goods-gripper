1fbc076db2f3ca0ad92e5dab3ecbda8a900111b4
Danuraha <95142326+Danuraha@users.noreply.github.com>
2022-05-22 13:12:35 +0530

    updated FlexFunctions

--- GloveControllerSystem/GloveControllerSystem/FlexFunctions.c
+++ GloveControllerSystem/GloveControllerSystem/FlexFunctions.c
@@ -1,23 +1,38 @@
-int  voltage[2];
-unsigned int a[];
-float* getVoltage()                 
+unsigned int a[2];
+
+
+int* getVoltage()
 {
-    ADCSRA=0b11000001; //start conversion
-    ADMUX=0b01000000;  //set input channel to read
-    while((ADCSRA & (0x10))==0);//wait for conversion
-    a[0]=ADCL|ADCH<<8;                                                                                      //   Dout = Vin / step size.
-     _delay_ms(10);
+	DDRA=0x00;
+	ADCSRA=0b10000001;
+	ADMUX=0b01000000;
+	ADCSRA |= (1 << ADSC);
+	int Ain;
+	int AinLow;
+
+	ADCSRA=0b11000001;
+	ADMUX=0b01000000;
+	while((ADCSRA & (0x10))==0);
+	_delay_us(10);
+	AinLow = (int)ADCL;
+	Ain = (int)ADCH*256;
+	a[0] = Ain+ AinLow;
+	
+	_delay_ms(10);
 
 
- //    //conversion saved in 10 bits
 
 
-    ADCSRA=0b11000001;
-    ADMUX=0b01000001;
-    while ((ADCSRA & (0x10))==0);
-     a[1]=ADCL|ADCH<<8;
-     _delay_ms(10);
-    return a;
+
+	ADCSRA=0b11000001;
+	ADMUX=0b01000001;
+	while ((ADCSRA & (0x10))==0);
+	_delay_us(10);
+	AinLow = (int)ADCL;
+	Ain= (int)ADCH*256;
+	a[1] = Ain+ AinLow;
+	_delay_ms(10);
+	return a;
 
 
 }
