e4b688933584ae914d60bf0b170e36192c056d96
Danuraha <95142326+Danuraha@users.noreply.github.com>
2022-05-23 20:34:13 +0530

    passing value mapped to 0 to 128

--- IntegrationFile.c
+++ IntegrationFile.c
@@ -88,13 +88,15 @@ void gripper()
     int digitalVoltage;
     digitalVoltage=(*getVoltage()+*(getVoltage()+2))/2;
 
-    if (digitalVoltage>=98)
+    if (digitalVoltage>=49)
     {
-      servoRight(0);
+      shrinkGripper;
     }  
     else
     {
-      servoLeft(0);
+      expandGripper;
     }                                                            //30 degree--50k----1.67V
     _delay_ms(500);                                               //45degree--62.5k---1.923V
-}
+}                                                                 //so 1.923V -----393.4458=394
+                                                                  //1023---394-----so,128-----49.298
+
