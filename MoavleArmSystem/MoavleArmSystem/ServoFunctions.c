10d2e378ab160a8acfebf1c401a27ee6c913080f
Danuraha <95142326+Danuraha@users.noreply.github.com>
2022-05-23 12:44:39 +0530

    final Updated-servoFunctions

--- MoavleArmSystem/MoavleArmSystem/ServoFunctions.c
+++ MoavleArmSystem/MoavleArmSystem/ServoFunctions.c
@@ -3,12 +3,13 @@
 #include <stdio.h>		/* Include std. library file */
 #include <util/delay.h>		/* Include Delay header file */
 
-unsigned int i;
-	DDRC=0xFF;
-	DDRD = 0b00100000;
 
+unsigned int i;
 void pwmsetup()
 {
+	
+	DDRC=0xFF;
+	DDRD = 0b00100000;
 	
 	TCNT1 = 0;		/* Set timer1 count zero */
 	ICR1 = 2499;		/* Set TOP count for timer1 in ICR1 register */
@@ -18,37 +19,40 @@ void pwmsetup()
 	TCCR1B = (1<<WGM12)|(1<<WGM13)|(1<<CS10)|(1<<CS11);
 }
 
-
+/*int main()
+{
+	
+}*/
 
 
 void rotateRight()
 {
 
-   for(i=127;i<=255;i++)
-		{
-			_delay_ms(20);
-			OCR1A = i;
-		}
+	for(i=127;i<=255;i++)
+	{
+		_delay_ms(20);
+		OCR1A = i;
+	}
 }
 
 
 void rotateLeft()
 {
-   for(i=255;i>=127;i--)
-		{
-			_delay_ms(20);
-			OCR1A = i;
-		}
+	for(i=255;i>=127;i--)
+	{
+		_delay_ms(20);
+		OCR1A = i;
+	}
 	
 }
 
 
 void stopGripper()
 {
-    PORTC=0x00;
-  PORTC=0xF6;
-  PORTD=0xDD;
-   
+	PORTC=0x00;
+	PORTC=0xF6;
+	PORTD=0xDD;
+	
 
 }
 
@@ -56,70 +60,70 @@ void stopGripper()
 
 void servo1Left()
 {
-    PORTC=0x03;
-    rotateLeft();
+	PORTC=0x03;
+	rotateLeft();
 }
 
 void servo1Right()
 {
-    PORTC=0x03;
-    rotateRight();
+	PORTC=0x03;
+	rotateRight();
 }
 void servo2Up()
 {
-    PORTC=0x02;
-    rotateRight();
+	PORTC=0x02;
+	rotateRight();
 }
 
 void servo2Down()
 {
-    PORTC=0x02;
-    rotateLeft();
+	PORTC=0x02;
+	rotateLeft();
 }
 
 void servo3Up()
 {
-    PORTC=0x01;
-    rotateRight();
+	PORTC=0x01;
+	rotateRight();
 }
 
 void servo3Down()
 {
-    PORTC=0x01;
-    rotateLeft();
+	PORTC=0x01;
+	rotateLeft();
 }
 
 void expandGripper()
 {
-    PORTC=0x00;
-    rotateRight();
+	PORTC=0x00;
+	rotateRight();
 }
 
 void shrinkGripper()
 {
-    PORTC=0x00;
-    rotateLeft();
+	PORTC=0x00;
+	rotateLeft();
 }
 
 void servo1Stop()
 {
-     PORTC=0x03;
-   PORTC=0xF6;
- 
+	PORTC=0x03;
+	PORTC=0xF6;
+	
 }
 
 void servo2Stop()
 {
-     PORTC=0x02;
-   PORTC=0xF6;
-   PORTD=0xDD;
- 
+	PORTC=0x02;
+	PORTC=0xF6;
+	PORTD=0xDD;
+	
 }
 
 void servo3Stop()
 {
-    PORTC=0x01;
-    PORTC=0xF6;
-    PORTD=0xDD;
+	PORTC=0x01;
+	PORTC=0xF6;
+	PORTD=0xDD;
 
 }
