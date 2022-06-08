503999e24bafcf3d6d68e8800e318bf682f9254c
Thareejan <59886309+tha-ree-jan@users.noreply.github.com>
2022-06-09 03:17:10 +0530

    void loop issue and if else issues resolved

--- MoavleArmSystem/MoavleArmSystem/main.c
+++ MoavleArmSystem/MoavleArmSystem/main.c
@@ -3,118 +3,135 @@
  *
  * Created: 5/13/2022 6:11:43 PM
  * Author : Thareejan
- */ 
+ */
 
+#define F_CPU 8000000UL /* define Clock Frequency */
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <util/delay.h>
+
+#include "ServoFunctions.c"
+#include "StepperFunctions.c"
+#include "UltrasonicFunction.c"
+#include "BluetoothRecevierFunction.c"
+#include "FsrFunction.c"
 
 void Mpu6050Stepper();
 void Mpu6050Servo();
 void gripper();
 
-
-	void loop()
-	{
-		if (ultrasonicValue()>10)
-		{
-				Mpu6050Stepper();
-				Mpu6050Servo();		
-		}
-		else
-		{
-			if (fsrValue()<200)
-			{
-				servoStop(1,128);
-				servoStop(2,128);
-				servoStop(3,128);
-				gripper();
-			}
-			else
-			{
-				if (*(getSensorValues()+7)==6)
-					servoStop(0,128);
-				else(*(getSensorValues()+7)==7)
-					gripper();	
-			}
-			Mpu6050Stepper();
-			Mpu6050Servo();	
-			
-			
-		}
-		
-
-	}
-	return 0;
+int main()
+{
+        if (ultrasonicValue() > 10)
+        {
+            Mpu6050Stepper();
+            Mpu6050Servo();
+        }
+        else
+        {
+            if (fsrValue() < 200)
+            {
+                servoStop(1, 128);
+                servoStop(2, 128);
+                servoStop(3, 128);
+                gripper();
+            }
+            else
+            {
+                int n = *getSensorValues() + 7;
+                if (n == 6)
+                {
+                    servoStop(0, 128);
+                }
+                else if (n == 7)
+                {
+                    gripper();
+                }
+            }
+            Mpu6050Stepper();
+            Mpu6050Servo();
+        }
+    
+     return 0;
 }
-
 
 void Mpu6050Stepper()
 {
-	if ((*getSensorValues()==5)&& (*(getSensorValues()+6))==0)
-	{
-		StepStop();
-	}
-	else if ((*getSensorValues()==1)&& (*(getSensorValues()+6))==0)
-	{
-		HorizontalStepRight();
-	}
-	else if((*getSensorValues()==2)&& (*(getSensorValues()+6))==0)
-	{
-		HorizontalStepLeft();
-	}
-	else if((*(getSensorValues()+2)==3)&& (*(getSensorValues()+6))==0)
-	{
-		VerticalStepUp();
-	}
-	else if((*(getSensorValues()+2)==4)&& (*(getSensorValues()+6))==0)
-	{
-		VerticalStepDown();
-	}
-	
+    if ((*getSensorValues() == 5) && (*(getSensorValues() + 6)) == 0)
+    {
+        StepStop();
+    }
+    if ((*getSensorValues() == 1) && (*(getSensorValues() + 6)) == 0)
+    {
+        HorizontalStepRight();
+    }
+    if ((*getSensorValues() == 2) && (*(getSensorValues() + 6)) == 0)
+    {
+        HorizontalStepLeft();
+    }
+    if ((*(getSensorValues() + 2) == 3) && (*(getSensorValues() + 6)) == 0)
+    {
+        VerticalStepUp();
+    }
+    if ((*(getSensorValues() + 2) == 4) && (*(getSensorValues() + 6)) == 0)
+    {
+        VerticalStepDown();
+    }
 }
 
 void Mpu6050Servo()
 {
-	if ((*(getSensorValues()+3)==5)&& (*(getSensorValues()+6))==1 )
-	{
-		servoStop(3,128);
-		servoStop(1,128);
-		servoStop(2,128);
-		
-
-	}
-	else if ((*getSensorValues()==1)&& (*(getSensorValues()+6))==1)
-	{
-		servoRight(3,128);
-	}
-	else if((*getSensorValues()==2)&& (*(getSensorValues()+6))==1)
-	{
-		servoLeft(3,128);
-	}
-	if((*(getSensorValues()+2)==3)&& (*(getSensorValues()+6))==1 && (*(getSensorValues()+7))==7)
-	{
-		servoRight(1,128);
-	}
-	else if((*(getSensorValues()+2)==4)&& (*(getSensorValues()+6))==1 && (*(getSensorValues()+7))==7)
-	{
-		servoLeft(1,128);
-	}
-	if((*(getSensorValues()+2)==3)&& (*(getSensorValues()+6))==1 && (*(getSensorValues()+7))==6)
-	{
-		servoRight(2,128);
-	}
-	else if((*(getSensorValues()+2)==4)&& (*(getSensorValues()+6))==1 && (*(getSensorValues()+7))==6)
-	{
-		servoLeft(2,128);
-	}
-
+    if ((*(getSensorValues() + 3) == 5) && (*(getSensorValues() + 6)) == 1)
+    {
+        servoStop(3, 128);
+        servoStop(1, 128);
+        servoStop(2, 128);
+    }
+    else if ((*getSensorValues() == 1) && (*(getSensorValues() + 6)) == 1)
+    {
+		servoStop(1, 128);
+		servoStop(2, 128);
+        servoRight(3, 128);
+    }
+    else if ((*getSensorValues() == 2) && (*(getSensorValues() + 6)) == 1)
+    {
+        servoStop(1, 128);
+        servoStop(2, 128);
+		servoLeft(3, 128);
+    }
+    if ((*(getSensorValues() + 2) == 3) && (*(getSensorValues() + 6)) == 1 && (*(getSensorValues() + 7)) == 7)
+    {
+        servoStop(3, 128);
+        servoStop(2, 128);
+		servoRight(1, 128);
+    }
+    else if ((*(getSensorValues() + 2) == 4) && (*(getSensorValues() + 6)) == 1 && (*(getSensorValues() + 7)) == 7)
+    {
+        servoStop(3, 128);
+        servoStop(2, 128);
+		servoLeft(1, 128);
+    }
+    if ((*(getSensorValues() + 2) == 3) && (*(getSensorValues() + 6)) == 1 && (*(getSensorValues() + 7)) == 6)
+    {
+        servoStop(1, 128);
+        servoStop(3, 128);
+		servoRight(2, 128);
+    }
+    else if ((*(getSensorValues() + 2) == 4) && (*(getSensorValues() + 6)) == 1 && (*(getSensorValues() + 7)) == 6)
+    {
+        servoLeft(2, 128);
+    }
 }
 
 void gripper()
 {
 
-
     int digitalVoltage;
-    digitalVoltage=(*getVoltage()+*(getVoltage()+2))/2;
-    servoRight(0,int(digitalVoltage)); 
-                                                             //30 degree--50k----1.67V
-    _delay_ms(500);                                               //45degree--62.5k---1.923V
+    digitalVoltage = (*(getSensorValues() + 4) + *(getSensorValues() + 5) / 2);
+    servoRight(0, digitalVoltage);
+    _delay_ms(500); 
 }
